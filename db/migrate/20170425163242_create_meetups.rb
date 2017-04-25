6aee93bc9932b48e1c1af203ea718e4bff9194fb
goldenhub777 <sg300c@gmail.com>
2017-04-25 12:58:19 -0400

    initial configuration for meetups in space

---
+++ db/migrate/20170425163242_create_meetups.rb
@@ -0,0 +1,14 @@
+class CreateMeetups < ActiveRecord::Migration
+  def change
+    create_table :meetups do |table|
+      table.string :name, null: false
+      table.string :location, null: false
+      table.string :creator, null: false
+      table.text :description, null: false
+      table.datetime :start_time, null: false
+      table.datetime :end_time, null: false
+
+      table.timestamps null: false
+    end
+  end
+end
